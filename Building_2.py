e085fb53a49ff78b1d4dc5c0ec25897e3a18d3e7
Boris <bvg99@mail.ru>
2024-03-03 15:13:48 +0700

    House

---
+++ Building_2.py
@@ -0,0 +1,13 @@
+class Building:
+
+    total = 0
+    def __init__(self):
+        Building.total += 1
+
+district = []
+district_size = 40
+while len(district) < district_size:
+    new_building = Building()
+    district.append(new_building)
+
+print(Building.total)
--- Probe.py
+++ Probe.py
@@ -32,19 +32,48 @@
 values = ['gold', 'silver', 'palladium', 'platinum']
 
 
-class Tainik:
-    def __init__(self, item=None):
-        self.content = []
-        if item is not None:
-            self.content.append(item)
-    def put(self, value):
-        self.content.extend(value)
-        print('added ', value)
-        print(self.content)
+# class Tainik:
+#     def __init__(self, item=None):
+#         self.content = []
+#         if item is not None:
+#             self.content.append(item)
+#     def put(self, value):
+#         self.content.extend(value)
+#         print('added ', value)
+#         print(self.content)
+#
+#
+# tainik = Tainik()
+#
+# tainik.put(values[2])
+#
+# print(len(self))
 
 
-tainik = Tainik()
+class SomeClass:
+    x = 67
 
-tainik.put(values[2])
+    def __init__(self):
+        self.x = 78
+    def method_one(self):
+        x = 23
+        print('method_one, x = ', x)
 
-print(len(self))
+    def method_two(self):
+        x = 34
+        def func():
+            x = 56
+            print('func, x = ', SomeClass.x)
+        func()
+        print('method_two, x = ', self.x)
+
+x = 12
+obj = SomeClass()
+obj.method_one()
+obj.method_two()
+print('global ', x)
+
+
+
+
+
